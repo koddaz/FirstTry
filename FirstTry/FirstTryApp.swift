22c579b2c7e9d30c0c381bbc99310b3eacaa0b2e
koddaz <Albert.niklasson@gmail.com>
2024-11-04 09:45:50 +0100

    Initial Commit

---
+++ FirstTry/FirstTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTryApp.swift
+//  FirstTry
+//
+//  Created by Albert Niklasson on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct FirstTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FirstTryTests/FirstTryTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTryTests.swift
+//  FirstTryTests
+//
+//  Created by Albert Niklasson on 2024-11-04.
+//
+
+import Testing
+@testable import FirstTry
+
+struct FirstTryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
